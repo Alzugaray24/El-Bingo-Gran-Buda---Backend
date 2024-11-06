223fa37a17a9d95e0aca4cb7c85f5554d22c735e
Matias <alzugaray1997@gmail.com>
2024-11-06 20:41:19 -0300

    db

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 import express from "express";
-import helloRoutes from "./routes/helloRoutes.js"; // Ruta de ejemplo
+import helloRoutes from "./routes/helloRoutes.js";
+import config from "./config/config.js";
+import MongoSingleton from "./db/database.js";
 
 const app = express();
 
 // Middleware para parsear JSON
 app.use(express.json());
 
+MongoSingleton.getInstance();
+
 // Rutas
 app.use("/", helloRoutes);
 
 // Definir el puerto en el que el servidor escuchará
-const PORT = 9090;
+const PORT = config.port;
 
 // Inicializar el servidor y hacerlo escuchar en el puerto
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
+
+// Definir un objeto de configuración con las variables de entorno
+const config = {
+  port: process.env.PORT || 8080, // Si no existe la variable PORT, usará el valor 8080
+  dbURI: process.env.DB_URI, // URI de la base de datos
+  // Puedes agregar más configuraciones aquí
+};
+
+export default config;
